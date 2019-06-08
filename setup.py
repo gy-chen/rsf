995b88f35fe95914e482889d7c45e509b5c3e70b
GYCHEN <gy.chen@gms.nutc.edu.tw>
2019-06-08 20:14:08 +0800

    init

---
+++ setup.py
@@ -0,0 +1,28 @@
+from io import open
+
+from setuptools import find_packages, setup
+
+with open("rsf/__init__.py", "r") as f:
+    for line in f:
+        if line.startswith("__version__"):
+            version = line.strip().split("=")[1].strip(" '\"")
+            break
+    else:
+        version = "0.0.1"
+
+REQUIRES = []
+
+setup(
+    name="rsf",
+    version=version,
+    description="wikitonary processing utils for build structure that like WordNet",
+    author="GYCHEN",
+    author_email="gy.chen@gms.nutc.edu.tw",
+    maintainer="GYCHEN",
+    maintainer_email="gy.chen@gms.nutc.edu.tw",
+    url="https://github.com/gy-chen/rfs",
+    install_requires=REQUIRES,
+    tests_require=["pytest"],
+    packages=find_packages(),
+    entry_points={},
+)
