35c3e8b9a0255636e62ba2de4cc8ea0cccfe7fc4
GYCHEN <gy.chen@gms.nutc.edu.tw>
2019-06-09 09:36:17 +0800

    setup enviroment

---
+++ rsf_js/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.ts',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+};
