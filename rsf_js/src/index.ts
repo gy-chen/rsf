433246698aa786a8cae6a7d7a9b17ee6a4a210fc
GYCHEN <gy.chen@gms.nutc.edu.tw>
2019-06-09 21:35:13 +0800

    fix exports and remove some exports

--- rsf_js/src/index.ts
+++ rsf_js/src/index.ts
@@ -1,15 +1,9 @@
-import BFS from './BFS';
-import Dictionary from './BFS';
-import DistanceCalculator from './DistanceCalculator';
-import Graph from './Graph';
+import Dictionary from './Dictionary';
 import RelatedSentenceFinder from './RelatedSentenceFinder';
 import * as utils from './utils';
 
 export {
-    BFS,
     Dictionary,
-    DistanceCalculator,
-    Graph,
     RelatedSentenceFinder,
     utils
 };
